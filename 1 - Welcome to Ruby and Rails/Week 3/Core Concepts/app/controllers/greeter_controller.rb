abf4f323614fb6839e92a71c6a24a7cab2afe1bf
anishSachdeva <anish_@outlook.com>
2018-04-06 10:29:23 +0530

    created dir in week 3 for httparty

--- 1 - Welcome to Ruby and Rails/Week 3/Core Concepts/app/controllers/greeter_controller.rb
+++ 1 - Welcome to Ruby and Rails/Week 3/Core Concepts/app/controllers/greeter_controller.rb
@@ -1,9 +1,16 @@
 class GreeterController < ApplicationController
+  attr_accessor :times_page_loaded
+
+  def initialize
+    @times_page_loaded = 0
+  end
+
   def hello
     @kids = ['anish', 'piyush', 'aviral', 'harshil']
+    @times_page_loaded += 1
   end
 
-  def goodbye
-
-  end
+  # def goodbye
+  #
+  # end
 end
--- 1 - Welcome to Ruby and Rails/Week 3/Core Concepts/config/routes.rb
+++ 1 - Welcome to Ruby and Rails/Week 3/Core Concepts/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'greeter#hello'
   get 'hello' => 'greeter#hello'
+  get 'goodbye' => 'greeter#goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
